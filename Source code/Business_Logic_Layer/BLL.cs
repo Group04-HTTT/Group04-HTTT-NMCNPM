c7963c762ee83c4bbc4812c9aba945203b066a0b
lthlong0310 <51038659+lthlong0310@users.noreply.github.com>
2019-12-15 01:36:10 +0700

    Add files via upload

---
+++ Source code/Business_Logic_Layer/BLL.cs
@@ -0,0 +1,25 @@
+﻿using Entity_Object;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Access_Layer;
+
+namespace Business_Logic_Layer
+{
+    public class BLL
+    {
+        public List<Staffs> get_Staff()
+        {
+            List<Staffs> kq = new List<Staffs>();
+            DAL respon = new DAL();
+            var Staff_List = respon.GetStaffFrom_Database();
+            foreach(var nv in Staff_List)
+            {
+                kq.Add(nv);
+            }
+            return kq;
+        }    
+    }
+}
