9d78e0b90ad6b3338450f9646e79b95dfd05d98b
lthlong0310 <51038659+lthlong0310@users.noreply.github.com>
2019-12-15 01:09:56 +0700

    Bill Details

--- Source code/Entity_Object/BillsDetail.cs
+++ Source code/Entity_Object/BillsDetail.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +9,16 @@ namespace Entity_Object
     public class BillsDetail
     {
         public BillsDetail() { }
-        public BillsDetail(int Bill_id, int Product_id, Nullable<int> Qty, Nullable<decimal> Unit_price)
+        public BillsDetail(int Bill_id, int Product_id, int Number_of_products, decimal Unit_price)
         {
             bill_id = Bill_id;
             product_id = Product_id;
-            qty = Qty;
+            number_of_products = Number_of_products;
             unit_price = Unit_price;
         }
         public int bill_id { get; set; }
         public int product_id { get; set; }
-        public Nullable<int> qty { get; set; }
-        public Nullable<decimal> unit_price { get; set; }
+        public int number_of_products { get; set; }
+        public decimal unit_price { get; set; }
     }
 }
