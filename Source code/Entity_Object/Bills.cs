2dd4d2a5ed1df27d44f6373976a758cd1506f9b5
lthlong0310 <51038659+lthlong0310@users.noreply.github.com>
2019-12-15 01:07:53 +0700

    Bills

--- Source code/Entity_Object/Bills.cs
+++ Source code/Entity_Object/Bills.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +9,22 @@ namespace Entity_Object
     public class Bills
     {
         public Bills() { }
-        public Bills(int Bill_id, Nullable<DateTime> Bill_createdate, Nullable<int> Total_amt, Nullable<decimal> Total_price, Nullable<decimal> Discount, Nullable<decimal> Total_money, Nullable<int> Mem_id)
+        public Bills(int Bill_id, DateTime Modified_date, decimal Total_amt, decimal Total_price, decimal Discount, decimal Total_money, int Staff_id)
         {
             bill_id = Bill_id;
-            bill_createdate = Bill_createdate;
+            modified_date = Modified_date;
             total_amt = Total_amt;
             total_price = Total_price;
             discount = Discount;
             total_money = Total_money;
-            mem_id = Mem_id;
+            staff_id = Staff_id;
         }
         public int bill_id { get; set; }
-        public Nullable<DateTime> bill_createdate { get; set; }
-        public Nullable<int> total_amt { get; set; } //Tổng số lượng đã mua
-        public Nullable<decimal> total_price { get; set; }
-        public Nullable<decimal> discount { get; set; }
-        public Nullable<decimal> total_money { get; set; }
-        public Nullable<int> mem_id { get; set; }
+        public DateTime modified_date { get; set; }
+        public decimal total_amt { get; set; } //Tổng số lượng đã mua
+        public decimal total_price { get; set; }
+        public decimal discount { get; set; }
+        public decimal total_money { get; set; }
+        public int staff_id { get; set; }
     }
 }
