e2ee2dc2f8f5fb5b55f39055a7bd6023e4350997
lthlong0310 <51038659+lthlong0310@users.noreply.github.com>
2019-12-15 01:24:18 +0700

    class Staff

--- Source code/Entity_Object/Staffs.cs
+++ Source code/Entity_Object/Staffs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +9,24 @@ namespace Entity_Object
     public class Staffs
     {
         public Staffs() { }
-        public Staffs(int Staff_id, string Staff_name, string Staff_phone, string Staff_addr, Nullable<DateTime> Staff_dob, Nullable<int> Staff_pos, string Staff_socialid, Nullable<int> Staff_gender, string Staff_pass)
+        public Staffs(int Staff_id, string Staff_name, string Staff_phone, string Staff_addr, DateTime Staff_dob, int Staff_type_id, string Staff_socialid, int Staff_gender)
         {
             staff_id = Staff_id;
             staff_name = Staff_name;
             staff_phone = Staff_phone;
-            staff_addr = Staff_addr;
+//            staff_addr = Staff_addr;
             staff_dob = Staff_dob;
-            staff_pos = Staff_pos;
+            staff_type_id = Staff_type_id;
             staff_socialid = Staff_socialid;
-            staff_gender = Staff_gender;
-            staff_pass = Staff_pass;
+            staff_gender = Staff_gender;       
         }
         public int staff_id { get; set; }
         public string staff_name { get; set; }
         public string staff_phone { get; set; }
-        public string staff_addr { get; set; }
-        public Nullable<DateTime> staff_dob { get; set; }
-        public Nullable<int> staff_pos { get; set; }
+//       public string staff_addr { get; set; }
+        public DateTime staff_dob { get; set; }
+        public int staff_type_id { get; set; }
         public string staff_socialid { get; set; }
-        public Nullable<int> staff_gender { get; set; }
-        public string staff_pass { get; set; }
+        public int staff_gender { get; set; }  // 0: Male 1: Female, 2: Other
     }
 }
